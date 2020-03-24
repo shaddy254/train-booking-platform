57636c827f7e053e8fba3a4d818cdc05102a6290
shaddy254 <shedrackn0@gmail.com>
2020-03-24 14:03:14 +0300

    added information on booking process

--- program.c
+++ program.c
@@ -7,19 +7,25 @@ void login();
 void mainEntryProject();
 int askForDestination();
 int askForDepart_point();
-int askForDayOfTravel();
 void bookTrain();
 void payForTrain();
 void assignTrainpay();
+void getCustomerDetails();
+void updateSeatsBooked(int number_of_seat_adult, int number_of_seats_child);
 
 
 int travel_time; 
+int number_of_seats_train = 500; //tracks the number remaining in train
+int total_seats_booked; //number of adults + number of chldren
+int number_of_seat; //number of travellers above 18 years
+int number_of_children; //number of travellers below 18 years
 char destination;
 char username[10];
 char password[10];
 int price = 1000;
 char departure_t[10] = "10.00 HR";
 char arrival_time[10] = "10.00 HR";
+
 
 
 int PICK_POINT, DEST_POINT;
@@ -33,6 +39,7 @@ const int DEST_POINT_NAIVA = 6;
 
 int main(){
     // login();
+    getCustomerDetails();
     mainEntryProject();
     return 0;
 }
@@ -71,7 +78,7 @@ void mainEntryProject(){
         break;
     case 5://FROM MTITO ANDEI
         assignTrainpay();
-        break;// FROMKIBWEZI
+        break;// FROM KIBWEZI
     case 6:
         assignTrainpay();
         break;
@@ -82,7 +89,7 @@ void mainEntryProject(){
         assignTrainpay();
         break;
            assignTrainpay();
-    case 9:// EXPRESS MOMBASA NAIROBI
+    case 9:// BOARDING AT NAIROBI
         assignTrainpay();
         break;
     default:
@@ -91,64 +98,40 @@ void mainEntryProject(){
     }
 }
 
-int askForDestination(){
-      printf("Enter your destination\n");
-        scanf("%d", &destination); //1-9
-        return destination;
-}
-
-int askForDepart_point(){
-    char name, destination;
-    int id_no, _time_day, pick_point;
-    printf("Enter your name : ");
-    scanf("%s", &name);
-    printf("Enter your ID : ");
-    scanf("%d", &id_no);
-    printf("Enter pick point: Enter 1 : MOMBASA\n Enter 2 : NAIROBI \n Enter 3 : NAIVASHA\n");
-    scanf("%d", &PICK_POINT);
-    return PICK_POINT;
-}
-
 void assignTrainpay(){
+
     switch (askForDestination())   { //return int
         case 1 /* constant-expression */: //DESITINO MISIANTYI
-            askForDayOfTravel();
             bookTrain();
-            payForTrain();
+            
             break;
         case 2:
-            askForDayOfTravel();
             bookTrain();
-            payForTrain();
+           
         break;
         case 3:
-            askForDayOfTravel();
             bookTrain();
-            payForTrain();
+            
         break;
 
         case 4:
-            askForDayOfTravel();
             bookTrain();
-            payForTrain();
+           
         break;
 
         case 5: 
-            askForDayOfTravel();
             bookTrain();
-            payForTrain();
+           
         break;
 
         case 6:
-            askForDayOfTravel();
             bookTrain();
-            payForTrain();
+            
         break;
 
         case 7:
-            askForDayOfTravel();
             bookTrain();
-            payForTrain();
+           
         break;
         
         default:
@@ -157,18 +140,64 @@ void assignTrainpay(){
 }
 
 void bookTrain(){
-    printf("Train Booked \n");
+    updateSeatsBooked;
+    if(number_of_seats_train < total_seats_booked){
+        printf("Sorry The train is fully booked. Try Another day");
+    }else{
+        payForTrain();
+        printf("Booking successful. Seat No. 53. Kindly Arrive 1 Hr BEFORE departre time");
+    }
+   
 }
 
 void payForTrain(){
-    printf("Kindly pay though MPesa.\n");
+    printf("Kindly pay though MPesa, Paybill number 685947, Account Number is Your ID Number\n");
+
 }
 
-int askForDayOfTravel(){
-    printf("Kindly Enter Day and time of Travel\n");
+int askForDestination(){
+    printf("Kindly Enter your destination\n 1 : MOMBASA\n 2 : MARIAKANI\n 3 : MIASENYI\n 4 : VOI\n 5 : MUTITO\n 6 : KIBWEZI\n 7 : EMALI\n 8 : ATHI RIVER\n 9 : NAIROBI \n");
+    scanf("%d", &destination); //1-9
+    return destination;
+}
+
+int askForDepart_point(){
+    printf("Kindly Enter your BOARDING POINT.  \n 1 : MOMBASA\n 2 : MARIAKANI\n 3 : MIASENYI\n 4 : VOI\n 5 : MUTITO\n 6 : KIBWEZI\n 7 : EMALI\n 8 : ATHI RIVER\n 9 : NAIROBI \n");
+    scanf("%d", &PICK_POINT);
+    return PICK_POINT;
 }
 
 
+void getCustomerDetails(){
+    char first_name, second_name; //first and surname
+    char gender; //male or female == F OR M
+    char destination; //
+    char pick_point; //
+    int id_no; //NATIONAL ID OR PASSPORT NUMBER
+    int  _hour, _minute; // (hh mm ss )
+    int _day, _month, _year; //(dd mm yyyyy)
+
+    printf("Enter your name : ");
+    scanf("%s %s", &first_name, &second_name);
+    printf("Enter your ID or PASSPORT NUMBER: ");
+    scanf("%d", &id_no);
+    printf("Enter your Gender : ");
+    scanf("%s", &gender);
+    printf("Enter Number Of Seats : ");
+    scanf("%d", &number_of_seat);
+    printf("Enter number Childdren below 18 Years Old : ");
+    scanf("%d", &number_of_children);
+    printf("Enter day of Travelling : ");
+    scanf("%d %d %d", &_day, &_month, &_year);
+    printf("Enter Time of Travelling : ");
+    scanf("%d %d", &_hour, &_minute);
+    
+}
+
+void updateSeatsBooked(int number_of_seat_adult, int number_of_seats_child){
+    number_of_seats_train - number_of_seat_adult - number_of_seats_child;
+    printf("%c", (char)number_of_seats_train);
+}
 
 
 
